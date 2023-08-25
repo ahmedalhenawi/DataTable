a4bf4eaec12690da5be5508b808fb4faed245280
ahmedalhenawi <ahmadelhenawi@gmail.com>
2023-08-25 13:46:18 +0300

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+// use Yajra\DataTables\DataTables;
+// // use Yajra\DataTables\Services\DataTable;
+use DataTables;
+class UserController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        if($request->ajax())
+        {
+            $data = User::select('*');
+
+            if($request->filled('from_date') && $request->filled('to_date'))
+            {
+                $data = $data->whereBetween('created_at', [$request->from_date, $request->to_date]);
+            }
+
+            return DataTables::of($data)->addIndexColumn()->make(true);
+        }
+        return view("user.index");
+    }
+
+}
